01ae7f0d696acf8e002a1212a895b332617f7a30
andrey <andrey.311@ya.ru>
2021-06-27 21:07:29 +0300

    dz10

---
+++ dz10/lesson10dz2.py
@@ -0,0 +1,32 @@
+
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+     def __init__(self, param):
+         self.param = param
+
+     @abstractmethod
+     def calculate(self):
+        pass
+
+
+class Coat(Clothes):
+
+    @property
+    def calculate(self):
+        return round((self.param / 6.5) + 0.5)
+
+
+class Suit(Clothes):
+
+    @property
+    def calculate(self):
+        return round((2 * self.param) + 0.3)
+
+
+
+coat = Coat(50)
+suit = Suit(160)
+print(coat.calculate)
+print(suit.calculate)
